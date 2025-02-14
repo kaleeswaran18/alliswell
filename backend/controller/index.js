a439f9a6e31a6ec71667f8eadcc938093b96157d
kaleeswaran18 <kaleesvk1997@gmail.com>
2025-02-14 05:43:15 -0500

    please workon it

--- backend/controller/index.js
+++ backend/controller/index.js
@@ -1123,7 +1123,7 @@ if(req.body.role=='executeofficer'){
     "data": {
         "phone": "6380319587",
         "email": "mansoor@1998gmail.com",
-         "phoneOtp": "2341"
+         "phoneOtp": "234176"
     }
           
         });
