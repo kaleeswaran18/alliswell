3f413dee65fc28240ffe7a1bc9550eaa239565f8
Harith <harith.kv47@gmail.com>
2025-03-15 17:38:00 +0530

    fix

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ var usersRouter = require('./routes/users');
 // var superAdminRouter = require('./routes/superAdmin');
 
 //import file in db connection 
-require('./utills/dbconnection')
+require('./utills/dbconnection') 
 
 var app = express();
 
