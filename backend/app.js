344ce3ec2ad9ab3c59981c8364efceaa2c3a3eb4
Harith <harith.kv47@gmail.com>
2025-03-16 13:24:23 +0530

    Fix formatting in app.js by adding a space after the cookieParser() call

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 
 
 
-app.use(cookieParser());
+app.use(cookieParser()); 
 // app.use(express.static(path.join(__dirname, 'public/images')));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 // app.use(formData.parse());
