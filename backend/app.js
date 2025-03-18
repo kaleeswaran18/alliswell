ca0bfd8ce1ba94b9649d620544bbc5a8e50877a6
kaleeswaran18 <kaleesvk1997@gmail.com>
2025-03-18 19:30:03 +0530

    check

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,8 @@ app.use(express.urlencoded({ extended: false }));
  
 app.use(cookieParser());  
 // app.use(express.static(path.join(__dirname, 'public/images')));
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
+// app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use('/images', express.static('images'));
 // app.use(formData.parse());
 
 app.get('/start', (req, res) => {  
